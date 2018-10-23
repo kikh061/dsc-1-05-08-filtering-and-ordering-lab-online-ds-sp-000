d93f1764b753879c4b7ecbb1c627a7ca61846012
Ahmad Arida <ahmad061@yahoo.com>
2018-10-22 21:21:49 -0500

    filtering and ordering lab done

--- selects.py
+++ selects.py
@@ -1,23 +1,23 @@
 def select_all_female_dogs_name_and_breed():
-    return ""
+    return "select name, breed from dogs where gender = 'F';"
 
 def select_all_dogs_names_in_alphabetical_order():
-    return ""
+    return "select name from dogs order by name asc;"
 
 def select_nameless_dog():
-    return ""
+    return "select * from dogs where name is null;"
 
 def select_hungry_dogs_name_and_breed_ordered_by_youngest_to_oldest():
-    return ""
+    return "select name, breed from dogs where hungry = 1 order by age ASC;"
 
 def select_name_age_and_temperament_of_oldest_dog():
-    return ""
+    return "select name, age, temperament from dogs order by age desc limit 1;"
 
 def select_name_and_age_of_three_youngest_dogs():
-    return ""
+    return "select name, age from dogs order by age asc limit 3;"
 
 def select_name_and_breed_of_dogs_between_age_five_and_ten_ordered_by_oldest_to_youngest():
-    return ""
+    return "select name, breed from dogs where age between 5 and 10 order by age desc;"
 
 def select_name_age_and_hungry_of_hungry_dogs_between_age_two_and_seven_in_alphabetical_order():
-    return ""
+    return "select name, age, hungry from dogs where age between 2 and 8 and hungry = 1 order by name asc;"
